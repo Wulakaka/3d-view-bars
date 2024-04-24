d832675ea5ef4aac435489ed0d4e436c637281e9
张伟峰 <zhang.weifeng@tslsmart.com>
2024-04-24 16:20:50 +0800

    feat(rxjs): 测试转换 subject 是否会堆积信息

--- src/views/RxjsView/RxjsView.vue
+++ src/views/RxjsView/RxjsView.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
-import { Subject, from, Observable } from 'rxjs'
+import { Subject, from, Observable, filter } from 'rxjs'
 import { onUnmounted } from 'vue'
 
 const subject = new Subject<number>()
+// 转换之后也不会堆积
+const o = subject.pipe(filter(() => true))
 
 // const observable = new Observable<number>(function subscribe(subscriber) {
 //   subscriber.next(3)
@@ -13,10 +15,10 @@ const subject = new Subject<number>()
 //     clearTimeout(timer)
 //   }
 // })
-const observable = from([4, 5])
-observable.subscribe(subject)
+// const observable = from([4, 5])
+// observable.subscribe(subject)
 
-const subscriptionA = subject.subscribe({
+const subscriptionA = o.subscribe({
   next: (v) => console.log(`observerA: ${v}`),
   complete() {
     console.log('observerA complete')
@@ -25,7 +27,7 @@ const subscriptionA = subject.subscribe({
 
 subject.next(1)
 
-const subscriptionB = subject.subscribe({
+const subscriptionB = o.subscribe({
   next: (v) => console.log(`observerB: ${v}`),
   complete() {
     console.log('observerB complete')
@@ -35,6 +37,25 @@ const subscriptionB = subject.subscribe({
 subject.next(2)
 
 subscriptionA.add(subscriptionB)
+
+subscriptionA.unsubscribe()
+subject.next(3)
+
+o.subscribe({
+  next: (v) => console.log(`observerA: ${v}`),
+  complete() {
+    console.log('observerA complete')
+  }
+})
+
+o.subscribe({
+  next: (v) => console.log(`observerB: ${v}`),
+  complete() {
+    console.log('observerB complete')
+  }
+})
+subject.next(4)
+
 onUnmounted(() => {
   subscriptionA.unsubscribe()
 })
